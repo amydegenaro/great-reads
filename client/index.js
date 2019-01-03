ee8da77ac44e15d4cde290ae27aa414f35017594
Amy De Genaro <amy.degenaro@gmail.com>
2019-01-03 16:35:01 -0500

    initial commit

---
+++ client/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+// import {BrowserRouter} from 'react-router-dom'
+
+import store from './store'
+import Root from './components/root'
+
+render(
+  <Provider store={store}>
+    {/* <BrowserRouter> */}
+      <Root />
+    {/* </BrowserRouter> */}
+  </Provider>,
+  document.getElementById('main')
+)
