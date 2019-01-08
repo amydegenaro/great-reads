ad908bcaadbb8c41fcccc0333ce39a9e281619ed
Amy De Genaro <amy.degenaro@gmail.com>
2019-01-07 20:38:40 -0500

    search box styling

--- client/components/searchBox.jsx
+++ client/components/searchBox.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const SearchBox = props => {
   return (
     <form onSubmit={props.handleSubmit}>
-      <label name="title">Find a book</label>
       <input name="title" value={props.title} onChange={props.handleChange} placeholder="Search by title"/>
-      <button type="submit">Search</button>
+      <button type="submit">Find Books</button>
     </form>
   )
 }
