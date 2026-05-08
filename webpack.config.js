d4e3e312d1a7e3d6c639a161d3c66f6bc6209bbc
Amy De Genaro <amy.degenaro@gmail.com>
2026-05-07 21:17:51 -0400

    refactor(files): add src folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
-  entry: ['@babel/polyfill', './client/App.jsx'],
+  entry: ['@babel/polyfill', './src/client/App.jsx'],
   output: {
-    path: path.join(__dirname, '/public'),
+    path: path.join(__dirname, '/src/public'),
     filename: 'bundle.js',
   },
   resolve: {
